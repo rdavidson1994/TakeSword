a57993f21e4e5ab3479620fcd5a06f9452af6c29
rdavidson <rdavidson@accusoft.com>
2020-05-04 14:47:40 -0400

    FormatBuilder -> new file

--- TakeSword/UtilityFunctions.cs
+++ TakeSword/UtilityFunctions.cs
@@ -1,36 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Linq;
 
 namespace TakeSword
 {
-    public class FormatBuilder
-    {
-        private List<FormattableString> formattableStrings;
-        public FormatBuilder()
-        {
-            formattableStrings = new List<FormattableString>();
-        }
-        public void Add(FormattableString formattableString)
-        {
-            formattableStrings.Add(formattableString);
-        }
-        public void AddLine(FormattableString formattableString)
-        {
-            if (formattableStrings.Count != 0)
-            {
-                formattableStrings.Add($"\n");
-            }
-            formattableStrings.Add(formattableString);
-        }
-        public FormattableString Build()
-        {
-            IEnumerable<object> allArgs = formattableStrings.SelectMany(f => f.GetArguments());
-            string joinedFormats = string.Join("", formattableStrings.Select(x => x.Format));
-            return FormattableStringFactory.Create(joinedFormats, allArgs.ToArray());
-        }
-    }
     public class UtilityFunctions
     {
         public static FormattableString FormatConcat(FormattableString first, FormattableString second)
